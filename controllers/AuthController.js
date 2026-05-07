440b1dbaf10c2ddfdc96148bbec2a24069d2d957
Naufal Baihaqi <noufalbaihaqi.2005@gmail.com>
2026-05-07 19:19:24 +0700

    force session save before redirect

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
   viewLogin: (req, res) => {
-    // 1. Tambahkan layout: false agar tidak dibungkus sidebar
-    // 2. Tambahkan error: null agar variabel error di ejs tidak undefined
     res.render('login', { layout: false, error: null });
   },
 
@@ -14,22 +12,25 @@ module.exports = {
       const user = await User.findOne({ where: { username } });
 
       if (!user) {
-        // Jangan pakai res.send, tapi kembalikan ke halaman login beserta pesan error
         return res.render('login', { layout: false, error: 'Username tidak ditemukan!' });
       }
 
-      // AKTIFKAN BCRYPT! Karena password di database dari Seeder tadi sudah teracak
       const isValid = await bcrypt.compare(password, user.password); 
 
       if (isValid) {
-        // Set session
         req.session.userId = user.id;
-        // Boleh ditambah flag ini biar lebih aman
-        req.session.isLoggedIn = true; 
+        req.session.isLoggedIn = true;
         
-        res.redirect('/dashboard');
+        // Paksa save session sebelum redirect
+        req.session.save((err) => {
+          if (err) {
+            console.error('Session save error:', err);
+            return res.render('login', { layout: false, error: 'Terjadi kesalahan sistem.' });
+          }
+          res.redirect('/dashboard');
+        });
+
       } else {
-        // Tampilkan pesan error di form login
         return res.render('login', { layout: false, error: 'Password salah!' });
       }
     } catch (error) {
