13848d95348e6a55ae91bbe8c52955aaefdb79b2
Naufal Baihaqi <noufalbaihaqi.2005@gmail.com>
2026-05-08 13:18:26 +0700

    fix JWT middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken');
-const SECRET = process.env.SESSION_SECRET || 'rahasia-spk-saw';
-
-const cekLogin = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.redirect('/login');
-  try {
-    req.user = jwt.verify(token, SECRET);
-    next();
-  } catch {
-    res.redirect('/login');
-  }
-};
-
-module.exports = cekLogin;
+const jwt = require('jsonwebtoken'); 
+const SECRET = process.env.SESSION_SECRET || 'rahasia-spk-saw'; 
+ 
+const cekLogin = (req, res, next) => { 
+  const token = req.cookies.token; 
+  if (!token) return res.redirect('/login'); 
+  try { 
+    req.user = jwt.verify(token, SECRET); 
+    next(); 
+  } catch { 
+    res.redirect('/login'); 
+  } 
+}; 
+ 
+module.exports = cekLogin; 
